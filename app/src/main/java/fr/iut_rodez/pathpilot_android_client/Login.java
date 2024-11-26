9170fb3f144676af7077bc4ee3fce016fbd6ca24
François SP <francois-sp@gmx.fr>
2024-11-26 10:54:47 +0100

    feat: add login activity to the app
    - Introduced a new Login activity.
    - Updated the manifest to export the Login activity.

---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/Login.java
@@ -0,0 +1,14 @@
+package fr.iut_rodez.pathpilot_android_client;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
