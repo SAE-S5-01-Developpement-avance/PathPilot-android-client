6091fb12a6feae50cc1c4498ed77bf9ffa654734
François SP <francois-sp@gmx.fr>
2024-11-26 14:32:22 +0100

    feat: add Popup utility for toast messages
    Introduced a new utility class to simplify showing toast messages.
    - Added methods for short and long duration toasts.
    - Encapsulated context handling within the class.

---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Popup.java
@@ -0,0 +1,29 @@
+package fr.iut_rodez.pathpilot_android_client.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+public class Popup {
+
+    private final Context context;
+
+    public Popup(Context context) {
+        this.context = context;
+    }
+
+    private void showToast(String message, int length) {
+        new Toast(context);
+        Toast.makeText(context, message, length).show();
+    }
+
+    public void showToastLong(String message) {
+        showToast(message, Toast.LENGTH_LONG);
+    }
+
+    public void showToastShort(String message) {
+        showToast(message, Toast.LENGTH_SHORT);
+    }
+
+
+}
