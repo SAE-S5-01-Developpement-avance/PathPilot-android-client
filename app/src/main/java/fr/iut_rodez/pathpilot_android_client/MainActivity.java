35de1d58de1facdf56aa39c64b7d5b6dd4145ffc
François SP <francois-sp@gmx.fr>
2024-12-03 14:57:51 +0100

    feat: add redirection to Login

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/MainActivity.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/MainActivity.java
@@ -1,8 +1,8 @@
 package fr.iut_rodez.pathpilot_android_client;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,8 +10,11 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import fr.iut_rodez.pathpilot_android_client.login.Login;
 
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
     private EditText firstName;
     private EditText lastName;
     private EditText latitude;
@@ -46,16 +49,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         labelMail = findViewById(R.id.label_mail);
         labelPassword = findViewById(R.id.label_password);
 
-        findViewById(R.id.sign_up_button).setOnClickListener(this);
+        findViewById(R.id.sign_up_button).setOnClickListener(v -> createAccount());
+        findViewById(R.id.link_sign_in).setOnClickListener(v -> gotoSignIn());
 
     }
 
     /**
      * Checks that the parameters entered are valid.
      * Creates an account or notifies the user of input errors.
-     * @param v
      */
-    public void onClick(View v) {
+    public void createAccount() {
         StringBuilder errorMessage = new StringBuilder();
         // reset the style of the text field
         resetFieldStyle();
@@ -74,114 +77,120 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         errorMessage.append(checkMail(mailText));
         errorMessage.append(checkPassword(passwordText));
 
-        if (errorMessage.length()!=0){
+        if (errorMessage.length() != 0) {
             Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
         } else {
-            sendInformationToSignInUser(firstNameText,lastNameText,latitudeText,longitudeText,mailText,passwordText);
+            sendInformationToSignInUser(firstNameText, lastNameText, latitudeText, longitudeText, mailText, passwordText);
         }
     }
 
     /**
      * Check the first name field.
+     *
      * @return errorMessage
      */
-    public String checkFirstName(String firstNameText){
+    public String checkFirstName(String firstNameText) {
 
         String errorMessage = "";
         if (firstNameText.isBlank()) {
             labelFirstName.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.first_name_blank);
+            errorMessage += getString(R.string.first_name_blank);
         }
         return errorMessage;
     }
 
     /**
      * Check the last name field.
+     *
      * @return errorMessage
      */
-    public String checkLastName(String lastNameText){
+    public String checkLastName(String lastNameText) {
         String errorMessage = "";
         if (lastNameText.isBlank()) {
             labelLastName.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.last_name_blank);
+            errorMessage += getString(R.string.last_name_blank);
         }
         return errorMessage;
     }
 
     /**
      * Check the latitude field.
+     *
      * @return errorMessage
      */
-    public String checkLatitude(String latitudeText){
+    public String checkLatitude(String latitudeText) {
         double latitudeValue = 0;
         String errorMessage = "";
         try {
             latitudeValue = Double.parseDouble(latitudeText);
             if (latitudeText.isBlank()) {
                 labelLatitude.setTextColor(getColor(R.color.red));
-                errorMessage+=getString(R.string.latitude_blank);
-            } else if (latitudeValue>=90 || latitudeValue<=-90) {
+                errorMessage += getString(R.string.latitude_blank);
+            } else if (latitudeValue >= 90 || latitudeValue <= -90) {
                 labelLatitude.setTextColor(getColor(R.color.red));
-                errorMessage+=getString(R.string.latitude_not_included);
+                errorMessage += getString(R.string.latitude_not_included);
             }
         } catch (Exception e) {
             labelLatitude.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.latitude_not_float);
+            errorMessage += getString(R.string.latitude_not_float);
         }
         return errorMessage;
     }
 
     /**
      * Check the longitude field.
+     *
      * @return errorMessage
      */
-    public String checkLongitude(String longitudeText){
+    public String checkLongitude(String longitudeText) {
         double longitudeValue = 0;
         String errorMessage = "";
         try {
             longitudeValue = Double.parseDouble(longitudeText);
             if (longitudeText.isBlank()) {
                 labelLongitude.setTextColor(getColor(R.color.red));
-                errorMessage+=getString(R.string.longitude_blank);
-            } else if (longitudeValue>=180 || longitudeValue<=-180) {
+                errorMessage += getString(R.string.longitude_blank);
+            } else if (longitudeValue >= 180 || longitudeValue <= -180) {
                 labelLongitude.setTextColor(getColor(R.color.red));
-                errorMessage+=getString(R.string.longitude_not_included);
+                errorMessage += getString(R.string.longitude_not_included);
             }
         } catch (Exception e) {
             labelLongitude.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.longitude_not_float);
+            errorMessage += getString(R.string.longitude_not_float);
         }
         return errorMessage;
     }
 
     /**
      * Check the mail field.
+     *
      * @return errorMessage
      */
-    public String checkMail(String mailText){
+    public String checkMail(String mailText) {
         String errorMessage = "";
         if (mailText.isBlank()) {
             labelMail.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.mail_blank);
+            errorMessage += getString(R.string.mail_blank);
         } else if (!mailText.matches(REGEX_MAIL)) {
             labelMail.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.mail_regex_invalided);
+            errorMessage += getString(R.string.mail_regex_invalided);
         }
         return errorMessage;
     }
 
     /**
      * Check the password field.
+     *
      * @return errorMessage
      */
-    public String checkPassword(String passwordText){
+    public String checkPassword(String passwordText) {
         String errorMessage = "";
         if (passwordText.isBlank()) {
             labelPassword.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.password_blank);
-        } else if (passwordText.length() <= PASSWORD_MIN_SIZE) {
+            errorMessage += getString(R.string.password_blank);
+        } else if (passwordText.length() < PASSWORD_MIN_SIZE) {
             labelPassword.setTextColor(getColor(R.color.red));
-            errorMessage+=getString(R.string.password_min_size_error);
+            errorMessage += getString(R.string.password_min_size_error);
         }
         return errorMessage;
     }
@@ -189,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * Reset the style of sign up interface.
      */
-    public void resetFieldStyle(){
+    public void resetFieldStyle() {
         labelFirstName.setTextColor(getColor(R.color.black));
         labelLastName.setTextColor(getColor(R.color.black));
         labelLatitude.setTextColor(getColor(R.color.black));
@@ -205,21 +214,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                             String latitudeText, String longitudeText,
                                             String mailText, String passwordText) {
         //TODO connect the API and send informations
-        Log.i("FIRSTNAME",firstNameText);
-        Log.i("LASTNAME",lastNameText);
-        Log.i("LATITUDE",Double.parseDouble(latitudeText)+"");
-        Log.i("LONGITUDE",Double.parseDouble(longitudeText)+"");
-        Log.i("MAIL",mailText);
-        Log.i("PASSWORD",passwordText);
+        Log.i("FIRSTNAME", firstNameText);
+        Log.i("LASTNAME", lastNameText);
+        Log.i("LATITUDE", Double.parseDouble(latitudeText) + "");
+        Log.i("LONGITUDE", Double.parseDouble(longitudeText) + "");
+        Log.i("MAIL", mailText);
+        Log.i("PASSWORD", passwordText);
 
         //TODO if API return an error, send an error message to the user returned by the API
     }
 
     /**
      * Sends to the sign in interface when the sign in Textview is clicked.
-     * @param v
      */
-    public void onClickSignIn(View v) {
-        //TODO go on the sign in interface
+    public void gotoSignIn() {
+        Log.d(TAG, "Switch to Login activity");
+        Intent intent = new Intent(this, Login.class);
+
+        // The user can't go back to the login activity by pressing the back button
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(intent);
     }
 }
