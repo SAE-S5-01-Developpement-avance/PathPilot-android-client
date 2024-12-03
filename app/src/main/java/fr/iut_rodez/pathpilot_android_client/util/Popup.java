83f186d096b51cedd5cdc646b76599fafc7e18ce
François SP <francois-sp@gmx.fr>
2024-12-03 18:32:43 +0100

    feat: enhance alert dialog functionality
    Added a new method to show customizable alert dialogs with optional buttons. Improved the existing showAlertDialog method to accept button parameters for more flexibility in user interactions.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Popup.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Popup.java
@@ -2,10 +2,12 @@ package fr.iut_rodez.pathpilot_android_client.util;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 /**
  * Instanceable class to display popups.
+ *
  * @author François de Saint Palais
  */
 public class Popup {
@@ -29,11 +31,61 @@ public class Popup {
         showToast(message, Toast.LENGTH_SHORT);
     }
 
+    /**
+     * Show an alert dialog with the given title and message.
+     * The dialog will have an OK button that dismisses the dialog.
+     *
+     * @param title   the title of the dialog
+     * @param message the message of the dialog
+     */
     public void showAlertDialog(String title, String message) {
-        new AlertDialog.Builder(context)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
-                .show();
+        Button okButton = new Button("OK", (dialog, which) -> dialog.dismiss());
+        showAlertDialog(title, message, okButton, null, null);
+    }
+
+
+    /**
+     * Show an alert dialog with the given title, message and onClickOKListener.
+     * If an onClickOKListener is provided, the dialog will have an OK button that dismisses the dialog.
+     *
+     * @param title          the title of the dialog
+     * @param message        the message of the dialog
+     * @param positiveButton the button to display
+     *                       with the text and the onClickListener
+     * @param neutralButton  the button to display
+     *                       with the text and the onClickListener
+     * @param negativeButton the button to display
+     *                       with the text and the onClickListener
+     */
+    public void showAlertDialog(String title, String message, Button positiveButton, Button neutralButton, Button negativeButton) {
+        AlertDialog.Builder dialog =
+                new AlertDialog.Builder(context)
+                        .setTitle(title)
+                        .setMessage(message);
+
+        if (positiveButton != null) {
+            dialog.setPositiveButton(positiveButton.text(), positiveButton.onClickListener());
+        }
+        if (neutralButton != null) {
+            dialog.setNeutralButton(neutralButton.text(), neutralButton.onClickListener());
+        }
+        if (negativeButton != null) {
+            dialog.setNegativeButton(negativeButton.text(), negativeButton.onClickListener());
+        }
+
+        // If no button is provided, add an OK button that dismisses the dialog
+        if (positiveButton == null && neutralButton == null && negativeButton == null) {
+            dialog.setPositiveButton("OK", (dialog1, which) -> dialog1.dismiss());
+        }
+
+        dialog.show();
+    }
+
+    /**
+     * Describe a button with a text and an onClickListener.
+     * @param text the text of the button
+     * @param onClickListener the onClickListener of the button
+     */
+    public record Button(String text, DialogInterface.OnClickListener onClickListener) {
     }
 }
