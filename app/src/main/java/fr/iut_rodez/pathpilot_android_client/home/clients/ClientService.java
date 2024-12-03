42be8f51b550f27a383c22187c68d20105a760d0
François SP <francois-sp@gmx.fr>
2024-12-04 00:21:14 +0100

    feat: add client management functionality
    - Introduced ClientService for API interactions.
    - Implemented method to fetch clients and display in a ListView.
    - Created FragmentClients to manage UI and handle user actions.
    - Added layout for the clients fragment with an add button.

---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/clients/ClientService.java
@@ -0,0 +1,84 @@
+package fr.iut_rodez.pathpilot_android_client.home.clients;
+
+import static fr.iut_rodez.pathpilot_android_client.util.Network.getRequestQueue;
+import static fr.iut_rodez.pathpilot_android_client.util.VolleyErrorHandler.handleError;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.iut_rodez.pathpilot_android_client.BuildConfig;
+import fr.iut_rodez.pathpilot_android_client.home.Home;
+import fr.iut_rodez.pathpilot_android_client.model.Client;
+import fr.iut_rodez.pathpilot_android_client.model.Client.ClientArrayAdapter;
+
+public class ClientService {
+
+    public static final String API_BASE_URL = BuildConfig.API_BASE_URL + "api/clients";
+    private static final String TAG = ClientService.class.getSimpleName();
+
+    private static RequestQueue requestQueue;
+
+    public static void getClients(Context context, ListView listClientsView) {
+        Log.d(TAG, "API URL: " + API_BASE_URL);
+
+        Home homeActivity = (Home) context;
+        requestQueue = getRequestQueue(context);
+        String tokenJWT = homeActivity.getTokenJWT();
+
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, API_BASE_URL, null,
+                response -> {
+                    progressDialog.dismiss();
+                    Log.d(TAG, "onResponse: " + response);
+                    try {
+                        JSONArray clients = response.getJSONObject("_embedded").getJSONArray("clientList");
+                        Log.d(TAG, "getClients: " + clients);
+
+                        List<Client> clientsArray = new ArrayList<>(clients.length());
+                        for (int i = 0; i < clients.length(); i++) {
+                            clientsArray.add((new Client(clients.getJSONObject(i))));
+                        }
+                        Log.d(TAG, "getClients: " + clientsArray);
+
+                        ClientArrayAdapter adapter = new ClientArrayAdapter(homeActivity, clientsArray);
+                        listClientsView.post(() -> {
+                            listClientsView.setAdapter(adapter);
+                        });
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                error -> {
+                    progressDialog.dismiss();
+                    Log.e(TAG, "onErrorResponse: ", error);
+                    handleError(context, error);
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + tokenJWT);
+                return headers;
+            }
+        };
+
+        requestQueue.add(request);
+    }
+}
--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/clients/FragmentClients.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/clients/FragmentClients.java
@@ -1,19 +1,66 @@
 package fr.iut_rodez.pathpilot_android_client.home.clients;
 
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 import fr.iut_rodez.pathpilot_android_client.R;
+import fr.iut_rodez.pathpilot_android_client.home.Home;
+import fr.iut_rodez.pathpilot_android_client.model.Client;
 
 public class FragmentClients extends Fragment {
 
+    /**
+     * The icon of the tab.
+     *
+     * @see fr.iut_rodez.pathpilot_android_client.home.Home
+     */
     public static final int ICON = R.drawable.icone_peoples;
+    private static final String TAG = FragmentClients.class.getSimpleName();
+
+    private ImageButton addClientButton;
+    private ListView listClientsView;
 
     public static FragmentClients newInstance() {
         return new FragmentClients();
     }
 
     private FragmentClients() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_clients, container, false);
+        Home homeActivity = (Home) getActivity();
+
+        addClientButton = view.findViewById(R.id.add_client);
+        listClientsView = view.findViewById(R.id.clients_list);
+
+        // Get the clients from the API
+        ClientService.getClients(homeActivity, listClientsView);
+
+        addClientButton.setOnClickListener(v -> gotoCreateClient());
+
+        return view;
+    }
+
+    private void gotoCreateClient() {
+        //TODO
+        Log.d(TAG, "gotoCreateClient: Goto create client");
     }
 }
