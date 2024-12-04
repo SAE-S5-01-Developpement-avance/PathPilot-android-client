f687f50ffdce9284ba71adb3d92200ddec76c3e3
François SP <francois-sp@gmx.fr>
2024-12-04 14:37:01 +0100

    feat: improve network connectivity check
    Enhanced the isNetworkConnected method to accurately determine internet connectivity. Added support for different Android versions, utilizing NetworkCapabilities for API level 29 and above, while maintaining compatibility with older versions by checking all networks.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/NetworkUtils.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/NetworkUtils.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -27,7 +28,22 @@ public class NetworkUtils {
      * @return true if the device is connected to the internet, false otherwise
      */
     public static boolean isNetworkConnected(Context context) {
-        return true; //STUB
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) return false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            NetworkCapabilities cap = cm.getNetworkCapabilities(cm.getActiveNetwork());
+            if (cap == null) return false;
+            return cap.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+        } else {
+            android.net.Network[] networks = cm.getAllNetworks();
+            for (android.net.Network n : networks) {
+                NetworkInfo nInfo = cm.getNetworkInfo(n);
+                if (nInfo != null && nInfo.isConnected()) return true;
+            }
+        }
+
+        return false;
     }
 
     public static NetworkUtils getInstance() {
