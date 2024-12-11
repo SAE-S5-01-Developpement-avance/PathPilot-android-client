b22fe6f61571dfc24c34f484d3d91e79c543dd94
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2024-12-11 16:54:29 +0100

    chore(Class Itinerary and AddItinerary):

---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/model/Itinerary.java
@@ -0,0 +1,45 @@
+package fr.iut_rodez.pathpilot_android_client.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Class representing an itinerary.
+ */
+public class Itinerary {
+    private int id;
+    private ArrayList<Client> clients;
+
+    public Itinerary(int id,ArrayList<Client> clients) {
+        this.id = id;
+        this.clients = clients;
+    }
+
+    public Itinerary (int id) {
+        this.id  = id;
+        this.clients = new ArrayList<>();
+    }
+
+    public Itinerary(JSONObject itineraryJson) throws JSONException {
+        //this.id = itineraryJson.getInt("");
+        //this.clients = itineraryJson.getJSONArray("");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<Client> clients) {
+        this.clients = clients;
+    }
+}
