2f86e8561ddbe39ef0ccdf8f1ca0395bbb921776
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2024-12-12 14:42:01 +0100

    chore(Ok to ad the API request):

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itineraries/AddItinerary.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itineraries/AddItinerary.java
@@ -3,19 +3,26 @@ package fr.iut_rodez.pathpilot_android_client.home.itineraries;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 import fr.iut_rodez.pathpilot_android_client.R;
+import fr.iut_rodez.pathpilot_android_client.home.clients.ClientService;
 import fr.iut_rodez.pathpilot_android_client.login.JWTToken;
 import fr.iut_rodez.pathpilot_android_client.home.clients.FragmentClients;
+import fr.iut_rodez.pathpilot_android_client.model.Client;
+import fr.iut_rodez.pathpilot_android_client.model.Itinerary;
 
 public class AddItinerary extends AppCompatActivity {
     private AutoCompleteTextView selectClientToAdd;
-    private Button createItinerary;
+    private ListView listClientsAddedView;
+    private ArrayList<Client> listClientsToAdd;
     private JWTToken jwtToken;
+    private ArrayList<Client> listClientsAdded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,12 @@ public class AddItinerary extends AppCompatActivity {
         setContentView(R.layout.view_create_itinerary);
 
         selectClientToAdd = findViewById(R.id.list_add_clients);
+        listClientsAddedView = findViewById(R.id.list_items_clients_added);
+
+        listClientsAdded = new ArrayList<>();
+        // TODO get all clients of the salesman and take into @listClientsToAdd
+        listClientsToAdd = new ArrayList<>();
+
         findViewById(R.id.button_create_itinerary).setOnClickListener(v -> createItinerary());
         Intent intent = getIntent();
         jwtToken = intent.getParcelableExtra(FragmentClients.CLE_TOKEN);
@@ -33,7 +46,16 @@ public class AddItinerary extends AppCompatActivity {
      * Create an itinerary with the clients selected.
      */
     public void createItinerary() {
-        // TODO Create itinerary
-        // TODO Create and send to the API
+        // TODO Send the Itinerary to the API
+        new Itinerary(listClientsAdded);
+        resetField();
+    }
+
+    /**
+     * Reset the content of "add itinerary" interface.
+     */
+    public void resetField() {
+        listClientsAddedView.removeAllViews();
+        selectClientToAdd.clearListSelection();
     }
 }
