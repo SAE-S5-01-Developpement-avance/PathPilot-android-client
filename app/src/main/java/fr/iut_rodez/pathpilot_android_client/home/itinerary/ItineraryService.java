b221070185d4a4b4cf99aecbd3b9f7bf3beed3a0
Florian Fabre <florian.97118@gmail.com>
2024-12-13 09:59:14 +0100

    feat: added itinerary and its service for list itineraries

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/ItineraryService.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/ItineraryService.java
@@ -5,11 +5,9 @@ import static fr.iut_rodez.pathpilot_android_client.util.network.NetworkUtils.ge
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 import android.widget.ListView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -24,57 +22,16 @@ import java.util.Map;
 
 import fr.iut_rodez.pathpilot_android_client.BuildConfig;
 import fr.iut_rodez.pathpilot_android_client.home.Home;
-import fr.iut_rodez.pathpilot_android_client.home.clients.AddClient;
 import fr.iut_rodez.pathpilot_android_client.home.itinerary.Itinerary.ItineraryArrayAdapter;
 
+/**
+ * Service to handle all itineraries related requests
+ */
 public class ItineraryService {
 
-    public static final String API_BASE_URL = BuildConfig.API_BASE_URL + "api/"; // TODO Complete the URL
+    public static final String API_BASE_URL = BuildConfig.API_BASE_URL + "api/routes";
     private static final String TAG = ItineraryService.class.getSimpleName();
-    /**
-     * Request to the API to add an itinerary.
-     * If the request is successful, it goes back to the previous activity.
-     * @param context Context of the application
-     * @param itinerary The itinerary to add
-     */
-    public static void addItinerary(Context context, Itinerary itinerary) {
-        Log.d(TAG, "API URL: " + API_BASE_URL);
 
-        AddItinerary addItineraryActivity = (AddItinerary) context;
-        RequestQueue requestQueue = getRequestQueue(context);
-        String jwtToken = addItineraryActivity.getJWTToken().getToken();
-
-        ProgressDialog progressDialog = new ProgressDialog(context);
-        progressDialog.show();
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, API_BASE_URL, itinerary.toJson(),
-                response -> {
-                    progressDialog.dismiss();
-                    Log.d(TAG, "onResponse: " + response);
-
-                    Intent returnIntent = new Intent();
-                    returnIntent.putExtra(AddClient.CLE_CLIENT_ADDED, true);
-                    addItineraryActivity.setResult(AddClient.RESULT_OK, returnIntent);
-
-                    addItineraryActivity.finish();
-                },
-                error -> {
-                    progressDialog.dismiss();
-                    Log.e(TAG, "onErrorResponse: ", error);
-                    handleError(context, error);
-                }
-        ) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Authorization", "Bearer " + jwtToken);
-                return headers;
-            }
-        };
-
-        requestQueue.add(request);
-    }
-    
     /**
      * Request to the API the itineraries.
      * If the request is successful, it add itineraries to the adapter and link them to the view
