a6722cb8042d3d7f09faa215d4ffa09768fef71e
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-01-23 16:06:13 +0100

    Revert "Merge branch 'feat/11-create-itinerary' of https://github.com/SAE-S5-01-Developpement-avance/PathPilot-android-client into feat/11-create-itinerary"
    This reverts commit 759b333d6c2576fb119c846e4f64b0e42de8d8f0, reversing
    changes made to e8250593a753501992cdb5ef2d07d1215b52be9d.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/AddItinerary.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/AddItinerary.java
@@ -57,6 +57,8 @@ public class AddItinerary extends AppCompatActivity {
         listClientsAdded = new ArrayList<>();
         listClientsToAdd = new ArrayList<>();
 
+        listClientsToAdd.add(new Client(getString(R.string.select_client_to_create_itinerary), 0, 0, "", true, "", "", ""));
+
         listClientsToAdd.addAll((ArrayList<Client>) intent.getSerializableExtra(FragmentItineraries.CLE_LIST_CLIENT));
 
         clientsToAddAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listClientsToAdd) {
@@ -84,12 +86,15 @@ public class AddItinerary extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (listClientsAdded.size() < 8) {
-                    if (position != AdapterView.INVALID_POSITION) {
+                    if (position != AdapterView.INVALID_POSITION && position != 0) {
                         Client selectedClient = listClientsToAdd.get(position);
                         listClientsAdded.add(selectedClient);
                         clientsAddedAdapter.notifyDataSetChanged();
                         listClientsToAdd.remove(position);
                         clientsToAddAdapter.notifyDataSetChanged();
+                        if (!listClientsToAdd.isEmpty()) {
+                            selectClientToAdd.setSelection(0);
+                        }
                     }
                 } else if (position != 0){
                     popup.showAlertDialog(getString(R.string.error_title),getString(R.string.error_max_clients_per_itinerary));
@@ -107,6 +112,8 @@ public class AddItinerary extends AppCompatActivity {
 
         jwtToken = intent.getParcelableExtra(FragmentItineraries.CLE_TOKEN);
     }
+
+
 
     /**
      * Create an itinerary with the clients selected.
