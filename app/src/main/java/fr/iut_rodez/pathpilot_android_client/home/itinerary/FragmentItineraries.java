3d81b9e7b37315baa8183e7f019a9846634f5183
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-01-24 11:38:06 +0100

    chore:Context menu on the itineraries
    Add the "Create route" option.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/FragmentItineraries.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/FragmentItineraries.java
@@ -188,4 +188,9 @@ public class FragmentItineraries extends Fragment {
         }
         return (super.onContextItemSelected(item));
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        new MenuInflater(getActivity()).inflate(R.menu.itinerary_context_menu, menu);
+    }
 }
