77c499e3b801597903463ef4bfea6fb776474b3d
François SP <francois-sp@gmx.fr>
2025-01-27 15:49:27 +0100

    doc: clean and add documentation

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Parser.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Parser.java
@@ -3,7 +3,6 @@ package fr.iut_rodez.pathpilot_android_client.util;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.NonUiContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,21 +20,28 @@ public class Parser {
 
     private static final String TAG = Parser.class.getSimpleName();
 
+    /**
+     * Parse the JSON response of the GET clients request and return a list of clients
+     * <p>
+     *     Request URL example:
+     *     <a href="http://localhost:8080/api/clients">/api/clients</a>
+     *
+     * @param response JSON response of the GET clients request
+     * @return List of clients parsed from the JSON response. If an error occurs, an empty list is returned
+     */
     @NonNull
     public static List<Client> getClientsPageable(JSONObject response) {
         ArrayList<Client> listClients = new ArrayList<>();
 
         // Parse the JSON response and create a list of clients
         try {
-            JSONObject embedded = response.getJSONObject("_embedded");
-            if (embedded.has("clientList")) {
-                JSONArray embeddedListClients = embedded.getJSONArray("clientList");
-                for (int i = 0; i < embeddedListClients.length(); i++) {
-                    JSONObject clientJson = embeddedListClients.getJSONObject(i);
+            JSONArray embeddedListClients = response
+                    .getJSONObject("_embedded")
+                    .getJSONArray("clientList");
 
-                    Client client = new Client(clientJson);
-                    listClients.add(client);
-                }
+            for (int i = 0; i < embeddedListClients.length(); i++) {
+                JSONObject clientJson = embeddedListClients.getJSONObject(i);
+                listClients.add(new Client(clientJson));
             }
         } catch (JSONException e) {
             Log.e(TAG, "Error while parsing the JSON response", e);
