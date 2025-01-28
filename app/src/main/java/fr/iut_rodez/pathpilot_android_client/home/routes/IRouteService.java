12aa69cd4daa322b0a058a2adbbf298e8ffb2f83
François SP <francois-sp@gmx.fr>
2025-01-28 16:17:01 +0100

    feat: create RouteService

---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/routes/IRouteService.java
@@ -0,0 +1,5 @@
+package fr.iut_rodez.pathpilot_android_client.home.routes;
+
+public interface IRouteService {
+    public void createRoute();
+}
---
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/routes/RouteService.java
@@ -0,0 +1,4 @@
+package fr.iut_rodez.pathpilot_android_client.home.routes;
+
+public class RouteService implements IRouteService {
+}
