eab80c37bfebfeb7fb8bfd1006306a9fe02c79ff
François SP <francois-sp@gmx.fr>
2025-01-31 10:04:06 +0100

    feat: add progress dialog and error handling
    - Introduced a progress dialog for better user feedback.
    - Added methods to show and dismiss the progress dialog.
    - Implemented an error dialog method for displaying error messages.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Popup.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/Popup.java
@@ -1,20 +1,41 @@
 package fr.iut_rodez.pathpilot_android_client.util;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.widget.Toast;
 
+import fr.iut_rodez.pathpilot_android_client.R;
+
 /**
  * Instanceable class to display popups.
- *
  */
 public class Popup {
 
     private final Context context;
 
+    private ProgressDialog progressDialog;
+
     public Popup(Context context) {
         this.context = context;
+    }
+
+    public void showProgressDialog(String title, String message) {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle(title);
+        progressDialog.setMessage(message);
+        progressDialog.show();
+    }
+
+    public void showProgressDialog() {
+        showProgressDialog("", "");
+    }
+
+    public void dismissProgressDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
     }
 
     private void showToast(CharSequence message, int length) {
@@ -82,7 +103,8 @@ public class Popup {
 
     /**
      * Describe a button with a text and an onClickListener.
-     * @param text the text of the button
+     *
+     * @param text            the text of the button
      * @param onClickListener the onClickListener of the button
      */
     public record Button(String text, DialogInterface.OnClickListener onClickListener) {
@@ -94,5 +116,13 @@ public class Popup {
 
     public static void showAlertDialog(Context context, String title, String message, Button positiveButton, Button neutralButton, Button negativeButton) {
         new Popup(context).showAlertDialog(title, message, positiveButton, neutralButton, negativeButton);
+    }
+
+    public void showAlertDialog(String title, String message, Button positiveButton) {
+        showAlertDialog(title, message, positiveButton, null, null);
+    }
+
+    public void showErrorDialog(String message) {
+        showAlertDialog(context.getString(R.string.error), message);
     }
 }
