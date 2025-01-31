196d7d877995de7226a04d85aee14ec3cc0ffc1c
François SP <francois-sp@gmx.fr>
2025-01-31 15:06:04 +0100

    feat: refactor dialog button listeners
    - Extracted dismiss and finish logic into separate methods.
    - Simplified button creation for better readability.
    - Improved code organization by using dedicated listener methods.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/popup/DialogButton.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/util/popup/DialogButton.java
@@ -13,22 +13,41 @@ public record DialogButton(String text, DialogInterface.OnClickListener onClickL
 
     /**
      * Create a button with the text "OK" that dismisses the dialog.
+     *
      * @return the button
      */
     public static DialogButton OKdismiss() {
-        return new DialogButton("OK", (dialog, which) -> dialog.dismiss());
+        return new DialogButton("OK", getDismissListener());
     }
 
 
     /**
      * Create a button with the text "OK" that dismisses the dialog and finish the activity.
+     *
      * @param activity the activity to finish
      * @return the button
      */
     public static DialogButton OKFinish(Activity activity) {
-        return new DialogButton("OK", (dialog, which) -> {
+        return new DialogButton("OK", getFinishListener(activity));
+    }
+
+    /**
+     * Return an onClickListener that dismisses the dialog.
+     * @return the onClickListener
+     */
+    public static DialogInterface.OnClickListener getDismissListener() {
+        return (dialog, which) -> dialog.dismiss();
+    }
+
+    /**
+     * Return an onClickListener that dismisses the dialog and finish the activity.
+     * @param activity the activity to finish
+     * @return the onClickListener
+     */
+    public static DialogInterface.OnClickListener getFinishListener(Activity activity) {
+        return (dialog, which) -> {
             dialog.dismiss();
             activity.finish();
-        });
+        };
     }
 }
