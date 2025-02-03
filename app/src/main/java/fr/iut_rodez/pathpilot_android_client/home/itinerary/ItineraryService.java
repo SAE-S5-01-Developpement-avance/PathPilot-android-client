6b98865114ba27f663f1293a9712a69ba83d15d8
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-02-03 15:47:30 +0100

    fix:Move the call to ORS and the algo to the API

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/ItineraryService.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/ItineraryService.java
@@ -18,9 +18,12 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import fr.iut_rodez.pathpilot_android_client.R;
@@ -60,11 +63,18 @@ public class ItineraryService extends AppCompatActivity{
         String jwtToken = addItineraryActivity.getJWTToken().getToken();
         JSONArray listIdClient = new JSONArray();
         JSONObject itinerariesInput = new JSONObject();
-
+        JSONArray clientsLocationsJson = new JSONArray();
         for (Client client : listClients) {
             listIdClient.put(client.getId());
         }
+
+        for (int i = 0; i < clientsLocations.size(); i++) {
+            clientsLocationsJson.put(new JSONArray(clientsLocations.get(i)));
+        }
+
         itinerariesInput.put("clients_schedule", listIdClient);
+        itinerariesInput.put("locations", clientsLocationsJson);
+        itinerariesInput.put("metrics", "distance");
 
         Popup popup = new Popup(context);
         popup.showProgressDialog(context.getString(R.string.progress_creating_itinerary));
