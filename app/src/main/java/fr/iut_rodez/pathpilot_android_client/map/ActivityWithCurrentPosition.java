03012180adea0a474c45c839b4c76b41126e0dcd
François SP <francois-sp@gmx.fr>
2025-02-07 14:30:15 +0100

    feat: improve ActivityWithCurrentPosition
    Refactor to enhance map activity functionality.
    - Changed from abstract to concrete class.
    - Added initialization for osmdroid configuration.
    - Integrated current position handling in lifecycle methods.

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/map/ActivityWithCurrentPosition.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/map/ActivityWithCurrentPosition.java
@@ -1,21 +1,50 @@
 package fr.iut_rodez.pathpilot_android_client.map;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
+
+import fr.iut_rodez.pathpilot_android_client.R;
+import fr.iut_rodez.pathpilot_android_client.util.map.CurrentPosition;
 
 /**
  * Describe an activity with a map.
+ * <p>
+ *     When a class extends this class, it must as a MapView with the id {@link #MAPVIEW_ID} in its layout.
+ *     <br>
+ *     Also the class need to implement the {@link Activity#onCreate(Bundle)} method and call the super method after the {@link Activity#setContentView(View)} method.
+ *     Like that:
+ *     <pre>
+ * {@code
+ * @Override
+ * protected void onCreate(Bundle savedInstanceState) {
+ *     setContentView(R.layout.view_map_selection);
+ *     super.onCreate(savedInstanceState);
+ *     // Your code
+ * }
+ * }
+ *    </pre>
+ * </p>
  */
-public abstract class ActivityWithCurrentPosition extends AppCompatActivity {
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+public class ActivityWithCurrentPosition extends AppCompatActivity {
+
+    public static final int MAPVIEW_ID = R.id.mapview;
 
     protected MapView mapView;
+    protected CurrentPosition currentPosition;
 
-    public MapView getMapView() {
-        return mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Important! Initialise the osmdroid configuration
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+        mapView = findViewById(MAPVIEW_ID);
+        currentPosition = new CurrentPosition(this);
     }
 
     @Override
@@ -24,6 +53,9 @@ public abstract class ActivityWithCurrentPosition extends AppCompatActivity {
         if (mapView != null) {
             // Needed for OSMdroid
             mapView.onResume();
+        }
+        if (currentPosition != null) {
+            currentPosition.onResume();
         }
     }
 
@@ -34,5 +66,17 @@ public abstract class ActivityWithCurrentPosition extends AppCompatActivity {
             // Needed for OSMdroid
             mapView.onPause();
         }
+        if (currentPosition != null) {
+            currentPosition.onPause();
+        }
+    }
+
+    public MapView getMapView() {
+        return mapView;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        currentPosition.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
