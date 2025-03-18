c4ac51912635c6e35ad5faa27cd59d5e0e1e9b14
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-03-18 09:00:28 +0100

    fix: Name of some view files

--- app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/FragmentItineraries.java
+++ app/src/main/java/fr/iut_rodez/pathpilot_android_client/home/itinerary/FragmentItineraries.java
@@ -120,7 +120,7 @@ public class FragmentItineraries extends Fragment {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        new MenuInflater(getActivity()).inflate(R.menu.itineray_context_menu, menu);
+        new MenuInflater(getActivity()).inflate(R.menu.itinerary_context_menu, menu);
     }
 
     @Override
